823efa4a14d3b37e3e799a9becd2f9da95300f81
Gürkan Yöyen <gyoyen@gmail.com>
2022-09-05 08:21:09 +0300

    made the input fields in 'login_screen' the same as in 'register_screen'.

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -12,6 +12,8 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final _formkey = GlobalKey<FormState>();
+
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
 
@@ -27,95 +29,119 @@ class _LoginScreenState extends State<LoginScreen> {
           child: Center(
             child: Padding(
               padding: const EdgeInsets.all(30),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const SizedBox(
-                    height: 50,
-                  ),
-                  const Icon(
-                    CupertinoIcons.lock_circle_fill,
-                    size: 120,
-                  ),
-                  const SizedBox(
-                    height: 25,
-                  ),
-                  TextField(
-                    controller: _emailController,
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: CustomDecoration("E-mail"),
-                    textInputAction: TextInputAction.next,
-                  ),
-                  const SizedBox(
-                    height: 15,
-                  ),
-                  TextField(
-                    controller: _passwordController,
-                    keyboardType: TextInputType.visiblePassword,
-                    obscureText: true,
-                    decoration: CustomDecoration("Password"),
-                    textInputAction: TextInputAction.done,
-                  ),
-                  const SizedBox(
-                    height: 15,
-                  ),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      minimumSize: const Size.fromHeight(50),
-                      backgroundColor: Colors.black,
-                      shape: const StadiumBorder(),
+              child: Form(
+                key: _formkey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const SizedBox(
+                      height: 50,
                     ),
-                    onPressed: () async {
-                      try {
-                        await FirebaseAuth.instance.signInWithEmailAndPassword(
-                            email: _emailController.text.trim(),
-                            password: _passwordController.text.trim());
-                      } on FirebaseAuthException catch (e) {
-                        final snackBar = SnackBar(
-                          content: Text(e.code),
-                        );
-                        ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                      }
-                    },
-                    child: const Text("Login"),
-                  ),
-                  const SizedBox(
-                    height: 15,
-                  ),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      minimumSize: const Size.fromHeight(50),
-                      backgroundColor: Colors.grey[400],
-                      shape: const StadiumBorder(),
+                    const Icon(
+                      CupertinoIcons.lock_circle_fill,
+                      size: 120,
                     ),
-                    onPressed: () async {
-                      Navigator.of(context).push(
-                        CupertinoPageRoute(
-                          fullscreenDialog: true,
-                          builder: (context) => const RegisterScreen(),
-                        ),
-                      );
-                    },
-                    child: const Text("Register"),
-                  ),
-                  const SizedBox(
-                    height: 15,
-                  ),
-                  Align(
-                    alignment: Alignment.bottomCenter,
-                    child: MaterialButton(
+                    const SizedBox(
+                      height: 25,
+                    ),
+                    TextFormField(
+                      controller: _emailController,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: customDecoration(
+                          "E-mail", const Icon(CupertinoIcons.mail)),
+                      textInputAction: TextInputAction.next,
+                      validator: (value) {
+                        if (value != null && value.isEmpty) {
+                          return "Required.";
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    TextFormField(
+                      controller: _passwordController,
+                      keyboardType: TextInputType.visiblePassword,
+                      obscureText: true,
+                      decoration: customDecoration(
+                          "Password", const Icon(CupertinoIcons.lock)),
+                      textInputAction: TextInputAction.done,
+                      validator: (value) {
+                        if (value != null && value.isEmpty) {
+                          return "Required.";
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: const Size.fromHeight(50),
+                        backgroundColor: Colors.black,
+                        shape: const StadiumBorder(),
+                      ),
+                      onPressed: () async {
+                        if (!_formkey.currentState!.validate()) {
+                          return;
+                        }
+                        _formkey.currentState!.save();
+
+                        try {
+                          await FirebaseAuth.instance
+                              .signInWithEmailAndPassword(
+                                  email: _emailController.text.trim(),
+                                  password: _passwordController.text.trim());
+                        } on FirebaseAuthException catch (e) {
+                          final snackBar = SnackBar(
+                            content: Text(e.code),
+                          );
+                          ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                        }
+                      },
+                      child: const Text("Login"),
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: const Size.fromHeight(50),
+                        backgroundColor: Colors.grey[400],
+                        shape: const StadiumBorder(),
+                      ),
                       onPressed: () async {
                         Navigator.of(context).push(
                           CupertinoPageRoute(
                             fullscreenDialog: true,
-                            builder: (context) => const ChangePasswordScreen(),
+                            builder: (context) => const RegisterScreen(),
                           ),
                         );
                       },
-                      child: const Text("Forgot your password? Click here."),
+                      child: const Text("Register"),
                     ),
-                  ),
-                ],
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    Align(
+                      alignment: Alignment.bottomCenter,
+                      child: MaterialButton(
+                        onPressed: () async {
+                          Navigator.of(context).push(
+                            CupertinoPageRoute(
+                              fullscreenDialog: true,
+                              builder: (context) =>
+                                  const ChangePasswordScreen(),
+                            ),
+                          );
+                        },
+                        child: const Text("Forgot your password? Click here."),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
@@ -124,7 +150,7 @@ class _LoginScreenState extends State<LoginScreen> {
     );
   }
 
-  InputDecoration CustomDecoration(String hint) {
+  InputDecoration customDecoration(String hint, Icon icn) {
     InputDecoration ind = InputDecoration(
       enabledBorder: OutlineInputBorder(
         borderSide: const BorderSide(color: Colors.white),
@@ -141,6 +167,7 @@ class _LoginScreenState extends State<LoginScreen> {
       hintText: hint,
       fillColor: Colors.grey[200],
       filled: true,
+      icon: icn,
     );
     return ind;
   }
